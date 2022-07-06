292655e952b3fcefe57d3cc3e689e5c04fd4988b
matt <mrfryslie@gmail.com>
2022-07-05 22:36:04 -0700

    Initial commit -- Program meant to be a learning tool about music -implemented C Ionian -next plan is to implement remaining modes

---
+++ main.py
@@ -0,0 +1,65 @@
+import random
+import turtle
+from scales import *
+
+screen = turtle.Screen()
+image = "Piano.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+Y_FLOOR_WHITE_KEY = -182
+Y_FLOOR_BLACK_KEY = -25
+Y_CEILING = 180
+Y_NOTENAME_LOCATION = 74
+
+def get_mouse_click_coor(x, y):
+    if -494 < x < -446 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-476, Y_NOTENAME_LOCATION, 'C', degree)
+    if -443 < x < -411 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-435, Y_NOTENAME_LOCATION, 'C#', degree)
+    if -407 < x < -377 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-397, Y_NOTENAME_LOCATION, 'D', degree)
+    if -372 < x < -341 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-364, Y_NOTENAME_LOCATION, 'D#', degree)
+    if -337 < x < -289 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-319, Y_NOTENAME_LOCATION, 'E', degree)
+    if -283 < x < -236 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-261, Y_NOTENAME_LOCATION, 'F', degree)
+    if -233 < x < -200 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-224, Y_NOTENAME_LOCATION, 'F#', degree)
+    if -196 < x < -165 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-185, Y_NOTENAME_LOCATION, 'G', degree)
+    if -161 < x < -131 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-153, Y_NOTENAME_LOCATION, 'G#', degree)
+    if -127 < x < -94 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-115, Y_NOTENAME_LOCATION, 'A', degree)
+    if -90 < x < -60 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-82, Y_NOTENAME_LOCATION, 'A#', degree)
+    if -55 < x < 8 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-41, Y_NOTENAME_LOCATION, 'B', degree)
+
+def write_notename(x, y, notename, degree):
+    if (notename == ionian[degree]):
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(x, y)
+        if len(notename) > 1:
+            t.color('white')
+        t.write(notename, font=("Arial", 12, "bold"))
+        screen.onclick(clear_screen)
+
+def clear_screen(x,y):
+    screen.clear()
+    screen.addshape(image)
+    turtle.shape(image)
+    new_question()
+
+def new_question():
+    global degree
+    degree = random.choice(list(ionian))
+    turtle.title(f"What is the {degree} of the C major scale")
+    turtle.onscreenclick(get_mouse_click_coor)
+
+new_question()
+turtle.mainloop()
---
+++ scales.py
@@ -0,0 +1,7 @@
+ionian = {'first': 'C', 'second': 'D', 'third': 'E', 'fourth': 'F', 'fifth': 'G', 'sixth': 'A', 'seventh': 'B'}
+dorian = {'first': 'D', 'second': 'E', 'third': 'F', 'fourth': 'G', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C'}
+phrygian = {'first': 'E', 'second': 'F', 'third': 'G', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C', 'seventh': 'D'}
+lydian = {'first': 'F', 'second': 'G', 'third': 'A', 'fourth': 'B', 'fifth': 'C', 'sixth': 'D', 'seventh': 'E'}
+mixolydian = {'first': 'G', 'second': 'A', 'third': 'B', 'fourth': 'C', 'fifth': 'D', 'sixth': 'E', 'seventh': 'F'}
+aeolian = {'first': 'A', 'second': 'B', 'third': 'C', 'fourth': 'D', 'fifth': 'E', 'sixth': 'F', 'seventh': 'G'}
+locrian = {'first': 'B', 'second': 'C', 'third': 'D', 'fourth': 'E', 'fifth': 'F', 'sixth': 'G', 'seventh': 'A'}
