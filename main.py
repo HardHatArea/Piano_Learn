fecaa28b24f898b348709b6736382332d8a2e26a
matt <mrfryslie@gmail.com>
2022-10-01 16:05:35 -0700

    -All keys and their modes are now implemented

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Y_FLOOR_BLACK_KEY = -25
 Y_CEILING = 180
 Y_NOTENAME_LOCATION = 74
 
-def get_mouse_click_coor(x, y):
+def get_mouse_click_coor_sharp_key(x, y):
     if -494 < x < -446 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
         write_notename(-476, Y_NOTENAME_LOCATION, 'C', degree)
     if -443 < x < -411 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
@@ -38,6 +38,32 @@ def get_mouse_click_coor(x, y):
     if -55 < x < 8 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
         write_notename(-41, Y_NOTENAME_LOCATION, 'B', degree)
 
+def get_mouse_click_coor_flat_key(x, y):
+    if -494 < x < -446 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-476, Y_NOTENAME_LOCATION, 'C', degree)
+    if -443 < x < -411 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-435, Y_NOTENAME_LOCATION, 'Db', degree)
+    if -407 < x < -377 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-397, Y_NOTENAME_LOCATION, 'D', degree)
+    if -372 < x < -341 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-364, Y_NOTENAME_LOCATION, 'Eb', degree)
+    if -337 < x < -289 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-319, Y_NOTENAME_LOCATION, 'E', degree)
+    if -283 < x < -236 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-261, Y_NOTENAME_LOCATION, 'F', degree)
+    if -233 < x < -200 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-224, Y_NOTENAME_LOCATION, 'Gb', degree)
+    if -196 < x < -165 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-185, Y_NOTENAME_LOCATION, 'G', degree)
+    if -161 < x < -131 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-153, Y_NOTENAME_LOCATION, 'Ab', degree)
+    if -127 < x < -94 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-115, Y_NOTENAME_LOCATION, 'A', degree)
+    if -90 < x < -60 and Y_FLOOR_BLACK_KEY < y < Y_CEILING:
+        write_notename(-82, Y_NOTENAME_LOCATION, 'Bb', degree)
+    if -55 < x < 8 and Y_FLOOR_WHITE_KEY < y < Y_CEILING:
+        write_notename(-41, Y_NOTENAME_LOCATION, 'B', degree)
+
 def write_notename(x, y, notename, degree):
     if (notename == degree):
         t = turtle.Turtle()
@@ -57,14 +83,17 @@ def clear_screen(x,y):
 
 def new_question():
     global degree
-    mode_number = random.choice(list(scales.mode.values())) #get random value of mode
-    mode_name = [k for k, v in scales.mode.items() if v == mode_number] # get name of mode using value in prev step
-    degree = random.choice(list(scales.keyOfC[mode_number]))
-    degree_letter = scales.keyOfC[mode_number]
-    mode_name = ''.join(mode_name)# convert list to string
-    turtle.title(f"What is the {degree} of the {mode_name} mode in the key of C")
-    degree = degree_letter[degree] # change degree from string to notename
-    turtle.onscreenclick(get_mouse_click_coor)
+    random_key = random.choice(list(scales.keys)) #choose random key
+    random_key_letter = random_key[0]
+    mode_name, mode_number = random.choice(list(scales.mode.items())) #choose random mode and it's value (i.e ionian->0)
+    degree = random.choice(list(random_key[mode_number]))
+    degree_letter = random_key[mode_number]
+    turtle.title(f"What is the {degree} of the {mode_name} mode in the key of {random_key_letter['first']}")
+    degree = degree_letter[degree] #change degree from string to notename
+    if '#' in degree:
+        turtle.onscreenclick(get_mouse_click_coor_sharp_key)
+    else:
+        turtle.onscreenclick(get_mouse_click_coor_flat_key)
 
 new_question()
 turtle.mainloop()
--- scales.py
+++ scales.py
@@ -1,17 +1,99 @@
-# ionian = {'first': 'C', 'second': 'D', 'third': 'E', 'fourth': 'F', 'fifth': 'G', 'sixth': 'A', 'seventh': 'B'}
-# dorian = {'first': 'D', 'second': 'E', 'third': 'F', 'fourth': 'G', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C'}
-# phrygian = {'first': 'E', 'second': 'F', 'third': 'G', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C', 'seventh': 'D'}
-# lydian = {'first': 'F', 'second': 'G', 'third': 'A', 'fourth': 'B', 'fifth': 'C', 'sixth': 'D', 'seventh': 'E'}
-# mixolydian = {'first': 'G', 'second': 'A', 'third': 'B', 'fourth': 'C', 'fifth': 'D', 'sixth': 'E', 'seventh': 'F'}
-# aeolian = {'first': 'A', 'second': 'B', 'third': 'C', 'fourth': 'D', 'fifth': 'E', 'sixth': 'F', 'seventh': 'G'}
-# locrian = {'first': 'B', 'second': 'C', 'third': 'D', 'fourth': 'E', 'fifth': 'F', 'sixth': 'G', 'seventh': 'A'}
-
 mode = {'ionian': 0, 'dorian': 1, 'phrygian': 2, 'lydian': 3, 'mixolydian': 4, 'aeolian': 5, 'locrian': 6}
 
 keyOfC = [{'first': 'C', 'second': 'D', 'third': 'E', 'fourth': 'F', 'fifth': 'G', 'sixth': 'A', 'seventh': 'B'},
-{'first': 'D', 'second': 'E', 'third': 'F', 'fourth': 'G', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C'},
-{'first': 'E', 'second': 'F', 'third': 'G', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C', 'seventh': 'D'},
-{'first': 'F', 'second': 'G', 'third': 'A', 'fourth': 'B', 'fifth': 'C', 'sixth': 'D', 'seventh': 'E'},
-{'first': 'G', 'second': 'A', 'third': 'B', 'fourth': 'C', 'fifth': 'D', 'sixth': 'E', 'seventh': 'F'},
-{'first': 'A', 'second': 'B', 'third': 'C', 'fourth': 'D', 'fifth': 'E', 'sixth': 'F', 'seventh': 'G'},
-{'first': 'B', 'second': 'C', 'third': 'D', 'fourth': 'E', 'fifth': 'F', 'sixth': 'G', 'seventh': 'A'}]
+          {'first': 'D', 'second': 'E', 'third': 'F', 'fourth': 'G', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C'},
+          {'first': 'E', 'second': 'F', 'third': 'G', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C', 'seventh': 'D'},
+          {'first': 'F', 'second': 'G', 'third': 'A', 'fourth': 'B', 'fifth': 'C', 'sixth': 'D', 'seventh': 'E'},
+          {'first': 'G', 'second': 'A', 'third': 'B', 'fourth': 'C', 'fifth': 'D', 'sixth': 'E', 'seventh': 'F'},
+          {'first': 'A', 'second': 'B', 'third': 'C', 'fourth': 'D', 'fifth': 'E', 'sixth': 'F', 'seventh': 'G'},
+          {'first': 'B', 'second': 'C', 'third': 'D', 'fourth': 'E', 'fifth': 'F', 'sixth': 'G', 'seventh': 'A'}]
+
+keyOfDb = [{'first': 'Db', 'second': 'Eb', 'third': 'F', 'fourth': 'Gb', 'fifth': 'Ab', 'sixth': 'Bb', 'seventh': 'C'},
+           {'first': 'Eb', 'second': 'F', 'third': 'Gb', 'fourth': 'Ab', 'fifth': 'Bb', 'sixth': 'C', 'seventh': 'Db'},
+           {'first': 'F', 'second': 'Gb', 'third': 'Ab', 'fourth': 'Bb', 'fifth': 'C', 'sixth': 'Db', 'seventh': 'Eb'},
+           {'first': 'Gb', 'second': 'Ab', 'third': 'Bb', 'fourth': 'C', 'fifth': 'Db', 'sixth': 'Eb', 'seventh': 'F'},
+           {'first': 'Ab', 'second': 'Bb', 'third': 'C', 'fourth': 'Db', 'fifth': 'Eb', 'sixth': 'F', 'seventh': 'Gb'},
+           {'first': 'Bb', 'second': 'C', 'third': 'Db', 'fourth': 'Eb', 'fifth': 'F', 'sixth': 'Gb', 'seventh': 'Ab'},
+           {'first': 'C', 'second': 'Db', 'third': 'Eb', 'fourth': 'F', 'fifth': 'Gb', 'sixth': 'Ab', 'seventh': 'Bb'}]
+
+keyOfD = [{'first': 'D', 'second': 'E', 'third': 'F#', 'fourth': 'G', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C#'},
+          {'first': 'E', 'second': 'F#', 'third': 'G', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C#', 'seventh': 'D'},
+          {'first': 'F#', 'second': 'G', 'third': 'A', 'fourth': 'B', 'fifth': 'C#', 'sixth': 'D', 'seventh': 'E'},
+          {'first': 'G', 'second': 'A', 'third': 'B', 'fourth': 'C#', 'fifth': 'D', 'sixth': 'E', 'seventh': 'F#'},
+          {'first': 'A', 'second': 'B', 'third': 'C#', 'fourth': 'D', 'fifth': 'E', 'sixth': 'F#', 'seventh': 'G'},
+          {'first': 'B', 'second': 'C#', 'third': 'D', 'fourth': 'E', 'fifth': 'F#', 'sixth': 'G', 'seventh': 'A'},
+          {'first': 'C#', 'second': 'D', 'third': 'E', 'fourth': 'F#', 'fifth': 'G', 'sixth': 'A', 'seventh': 'B'}]
+
+keyOfEb = [{'first': 'Eb', 'second': 'F', 'third': 'G', 'fourth': 'Ab', 'fifth': 'Bb', 'sixth': 'C', 'seventh': 'D'},
+           {'first': 'F', 'second': 'G', 'third': 'Ab', 'fourth': 'Bb', 'fifth': 'C', 'sixth': 'D', 'seventh': 'Eb'},
+           {'first': 'G', 'second': 'Ab', 'third': 'Bb', 'fourth': 'C', 'fifth': 'D', 'sixth': 'Eb', 'seventh': 'F'},
+           {'first': 'Ab', 'second': 'Bb', 'third': 'C', 'fourth': 'D', 'fifth': 'Eb', 'sixth': 'F', 'seventh': 'G'},
+           {'first': 'Bb', 'second': 'C', 'third': 'D', 'fourth': 'Eb', 'fifth': 'F', 'sixth': 'G', 'seventh': 'Ab'},
+           {'first': 'C', 'second': 'D', 'third': 'Eb', 'fourth': 'F', 'fifth': 'G', 'sixth': 'Ab', 'seventh': 'Bb'},
+           {'first': 'D', 'second': 'Eb', 'third': 'F', 'fourth': 'G', 'fifth': 'Ab', 'sixth': 'Bb', 'seventh': 'C'}]
+
+keyOfE = [{'first': 'E', 'second': 'F#', 'third': 'G#', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C#', 'seventh': 'D#'},
+          {'first': 'F#', 'second': 'G#', 'third': 'A', 'fourth': 'B', 'fifth': 'C#', 'sixth': 'D#', 'seventh': 'E'},
+          {'first': 'G#', 'second': 'A', 'third': 'B', 'fourth': 'C#', 'fifth': 'D#', 'sixth': 'E', 'seventh': 'F#'},
+          {'first': 'A', 'second': 'B', 'third': 'C#', 'fourth': 'D#', 'fifth': 'E', 'sixth': 'F#', 'seventh': 'G#'},
+          {'first': 'B', 'second': 'C#', 'third': 'D#', 'fourth': 'E', 'fifth': 'F#', 'sixth': 'G#', 'seventh': 'A'},
+          {'first': 'C#', 'second': 'D#', 'third': 'E', 'fourth': 'F#', 'fifth': 'G#', 'sixth': 'A', 'seventh': 'B'},
+          {'first': 'D#', 'second': 'E', 'third': 'F#', 'fourth': 'G#', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C#'}]
+
+keyOfF = [{'first': 'E', 'second': 'F#', 'third': 'G#', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C#', 'seventh': 'D#'},
+          {'first': 'F#', 'second': 'G#', 'third': 'A', 'fourth': 'B', 'fifth': 'C#', 'sixth': 'D#', 'seventh': 'E'},
+          {'first': 'G#', 'second': 'A', 'third': 'B', 'fourth': 'C#', 'fifth': 'D#', 'sixth': 'E', 'seventh': 'F#'},
+          {'first': 'A', 'second': 'B', 'third': 'C#', 'fourth': 'D#', 'fifth': 'E', 'sixth': 'F#', 'seventh': 'G#'},
+          {'first': 'B', 'second': 'C#', 'third': 'D#', 'fourth': 'E', 'fifth': 'F#', 'sixth': 'G#', 'seventh': 'A'},
+          {'first': 'C#', 'second': 'D#', 'third': 'E', 'fourth': 'F#', 'fifth': 'G#', 'sixth': 'A', 'seventh': 'B'},
+          {'first': 'D#', 'second': 'E', 'third': 'F#', 'fourth': 'G#', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C#'}]
+
+keyOfGb = [{'first': 'Gb', 'second': 'Ab', 'third': 'Bb', 'fourth': 'C', 'fifth': 'Db', 'sixth': 'Eb', 'seventh': 'F'},
+          {'first': 'Ab', 'second': 'Bb', 'third': 'C', 'fourth': 'Db', 'fifth': 'Eb', 'sixth': 'F', 'seventh': 'Gb'},
+          {'first': 'Bb', 'second': 'C', 'third': 'Db', 'fourth': 'Eb', 'fifth': 'F', 'sixth': 'Gb', 'seventh': 'Ab'},
+          {'first': 'C', 'second': 'Db', 'third': 'Eb', 'fourth': 'F', 'fifth': 'Gb', 'sixth': 'Ab', 'seventh': 'Bb'},
+          {'first': 'Db', 'second': 'Eb', 'third': 'F', 'fourth': 'Gb', 'fifth': 'Ab', 'sixth': 'Bb', 'seventh': 'C'},
+          {'first': 'Eb', 'second': 'F', 'third': 'Gb', 'fourth': 'Ab', 'fifth': 'Bb', 'sixth': 'C', 'seventh': 'Db'},
+          {'first': 'F', 'second': 'Gb', 'third': 'Ab', 'fourth': 'Bb', 'fifth': 'C', 'sixth': 'Db', 'seventh': 'Eb'}]
+
+keyOfG = [{'first': 'G', 'second': 'A', 'third': 'B', 'fourth': 'C', 'fifth': 'D', 'sixth': 'E', 'seventh': 'F#'},
+          {'first': 'A', 'second': 'B', 'third': 'C', 'fourth': 'D', 'fifth': 'E', 'sixth': 'F#', 'seventh': 'G'},
+          {'first': 'B', 'second': 'C', 'third': 'D', 'fourth': 'E', 'fifth': 'F#', 'sixth': 'G', 'seventh': 'A'},
+          {'first': 'C', 'second': 'D', 'third': 'E', 'fourth': 'F#', 'fifth': 'G', 'sixth': 'A', 'seventh': 'B'},
+          {'first': 'D', 'second': 'E', 'third': 'F#', 'fourth': 'G', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C'},
+          {'first': 'E', 'second': 'F#', 'third': 'G', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C', 'seventh': 'D'},
+          {'first': 'F#', 'second': 'G', 'third': 'A', 'fourth': 'B', 'fifth': 'C', 'sixth': 'D', 'seventh': 'E'}]
+
+keyOfAb = [{'first': 'Ab', 'second': 'Bb', 'third': 'C', 'fourth': 'Db', 'fifth': 'Eb', 'sixth': 'F', 'seventh': 'G'},
+          {'first': 'Bb', 'second': 'C', 'third': 'Db', 'fourth': 'Eb', 'fifth': 'F', 'sixth': 'G', 'seventh': 'Ab'},
+          {'first': 'C', 'second': 'Db', 'third': 'Eb', 'fourth': 'F', 'fifth': 'G', 'sixth': 'Ab', 'seventh': 'Bb'},
+          {'first': 'Db', 'second': 'Eb', 'third': 'F', 'fourth': 'G', 'fifth': 'Ab', 'sixth': 'Bb', 'seventh': 'C'},
+          {'first': 'Eb', 'second': 'F', 'third': 'G', 'fourth': 'Ab', 'fifth': 'Bb', 'sixth': 'C', 'seventh': 'Db'},
+          {'first': 'F', 'second': 'G', 'third': 'Ab', 'fourth': 'Bb', 'fifth': 'C', 'sixth': 'Db', 'seventh': 'Eb'},
+          {'first': 'G', 'second': 'Ab', 'third': 'Bb', 'fourth': 'C', 'fifth': 'Db', 'sixth': 'Eb', 'seventh': 'F'}]
+
+keyOfA = [{'first': 'A', 'second': 'B', 'third': 'C#', 'fourth': 'D', 'fifth': 'E', 'sixth': 'F#', 'seventh': 'G#'},
+          {'first': 'B', 'second': 'C#', 'third': 'D', 'fourth': 'E', 'fifth': 'F#', 'sixth': 'G#', 'seventh': 'A'},
+          {'first': 'C#', 'second': 'D', 'third': 'E', 'fourth': 'F#', 'fifth': 'G#', 'sixth': 'A', 'seventh': 'B'},
+          {'first': 'D', 'second': 'E', 'third': 'F#', 'fourth': 'G#', 'fifth': 'A', 'sixth': 'B', 'seventh': 'C#'},
+          {'first': 'E', 'second': 'F#', 'third': 'G#', 'fourth': 'A', 'fifth': 'B', 'sixth': 'C#', 'seventh': 'D'},
+          {'first': 'F#', 'second': 'G#', 'third': 'A', 'fourth': 'B', 'fifth': 'C#', 'sixth': 'D', 'seventh': 'E'},
+          {'first': 'G#', 'second': 'A', 'third': 'B', 'fourth': 'C#', 'fifth': 'D', 'sixth': 'E', 'seventh': 'F#'}]
+
+keyOfBb = [{'first': 'Bb', 'second': 'C', 'third': 'D', 'fourth': 'Eb', 'fifth': 'F', 'sixth': 'G', 'seventh': 'A'},
+          {'first': 'C', 'second': 'D', 'third': 'Eb', 'fourth': 'F', 'fifth': 'G', 'sixth': 'A', 'seventh': 'Bb'},
+          {'first': 'D', 'second': 'Eb', 'third': 'F', 'fourth': 'G', 'fifth': 'A', 'sixth': 'Bb', 'seventh': 'C'},
+          {'first': 'Eb', 'second': 'F', 'third': 'G', 'fourth': 'A', 'fifth': 'Bb', 'sixth': 'C', 'seventh': 'D'},
+          {'first': 'F', 'second': 'G', 'third': 'A', 'fourth': 'Bb', 'fifth': 'C', 'sixth': 'D', 'seventh': 'Eb'},
+          {'first': 'G', 'second': 'A', 'third': 'Bb', 'fourth': 'C', 'fifth': 'D', 'sixth': 'Eb', 'seventh': 'F'},
+          {'first': 'A', 'second': 'Bb', 'third': 'C', 'fourth': 'D', 'fifth': 'Eb', 'sixth': 'F', 'seventh': 'G'}]
+
+keyOfB = [{'first': 'B', 'second': 'C#', 'third': 'D#', 'fourth': 'E', 'fifth': 'F#', 'sixth': 'G#', 'seventh': 'A#'},
+          {'first': 'C#', 'second': 'D#', 'third': 'E', 'fourth': 'F#', 'fifth': 'G#', 'sixth': 'A#', 'seventh': 'B'},
+          {'first': 'D#', 'second': 'E', 'third': 'F#', 'fourth': 'G#', 'fifth': 'A#', 'sixth': 'B', 'seventh': 'C#'},
+          {'first': 'E', 'second': 'F#', 'third': 'G#', 'fourth': 'A#', 'fifth': 'B', 'sixth': 'C#', 'seventh': 'D#'},
+          {'first': 'F#', 'second': 'G#', 'third': 'A#', 'fourth': 'B', 'fifth': 'C#', 'sixth': 'D#', 'seventh': 'E'},
+          {'first': 'G#', 'second': 'A#', 'third': 'B', 'fourth': 'C#', 'fifth': 'D#', 'sixth': 'E', 'seventh': 'F#'},
+          {'first': 'A#', 'second': 'B', 'third': 'C#', 'fourth': 'D#', 'fifth': 'E', 'sixth': 'F#', 'seventh': 'G#'}]
+
+keys = [keyOfC, keyOfDb, keyOfD, keyOfEb, keyOfE, keyOfF, keyOfGb, keyOfG, keyOfAb, keyOfA, keyOfBb, keyOfB]
